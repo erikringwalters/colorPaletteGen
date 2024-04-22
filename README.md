6a5935ffe35d4822a4a7a5b170e0a0946c8744a3
Erik Ring-Walters <erik.ring.walters@gmail.com>
2024-04-21 21:33:23 -0700

    better usability

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # colorPaletteGen
 A color palette generator created to offer a wide selection of colors in a texture image, for game assets using low-poly UV editing.
+
+# How to Use:
+From command line, run:
+python palette.py
+
+# How to adjust
+Open python file palette.py and change "user-adjusted parameters"
+
--- palette.py
+++ palette.py
@@ -7,23 +7,31 @@
 from PIL import Image
 from datetime import datetime
 
+# user-adjusted parameters
 SIZE=64
 MAX=255
 LEVELS=4
+
 LEVEL_SIZE=SIZE/LEVELS
 
 w=SIZE
 m=MAX
 l=LEVEL_SIZE
 
+print("Creating " + str(SIZE) + "px sized palette...")
+
 def normalized(x, min, max):
     return (x-min)/(max-min)*m
 
 def savePng(img, size, levels):
     now = datetime.now()
-    date_time = now.strftime("%Y-%m-%d_%H,%M,%S")
-    fn = "palette" + str(size) + "W_" + str(levels) + "L_" + date_time + ".png"
-
+    date_time = now.strftime("%Y-%m-%d_%H:%M:%S")
+    fn = "palette" + str(size)
+    ## uncomment for more detailed filenames
+    # fn = fn + "_" + str(levels) + "L"
+    # fn = fn + "_" + date_time
+    fn = fn + ".png"
+    print("Saving: " + fn)
     img.convert('RGB').save(fn)
 
 # minimum brightness
@@ -61,6 +69,10 @@ for i in range(w):
             
         img.putpixel((i,j), (h,s,b))
 
+## comment to prevent saving image
 savePng(img, SIZE, LEVELS)
 
+## comment to prevent showing image
 img.show()
+
+print("Done.")
